a5eaa6f3eec5687ff0c43f0293d3b24e966151bd
AndriesJ <anneries83@gmail.com>
2026-02-18 14:15:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = 'hike-cache-v1';
+const CACHE_NAME = 'hike-v2';
 const ASSETS = [
-    'index.html',
-    'manifest.json',
+    './',
+    './index.html',
+    './manifest.json',
     'https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.js',
     'https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.css'
 ];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then(res => {
-            return res || fetch(e.request).then(networkRes => {
+            return res || fetch(e.request).then(netRes => {
                 return caches.open(CACHE_NAME).then(cache => {
-                    // Cache map tiles as they are requested
                     if (e.request.url.includes('tile.openstreetmap.org')) {
-                        cache.put(e.request, networkRes.clone());
+                        cache.put(e.request, netRes.clone());
                     }
-                    return networkRes;
+                    return netRes;
                 });
             });
         })
